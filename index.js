86112a8870c48e9480fd79b30905da44c03dc18e
RajnishPandeyTeqfocus <130439359+RajnishPandeyTeqfocus@users.noreply.github.com>
2024-02-02 00:31:10 +0530

    enable cors for all routes

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.json());
+app.use(cors());
 
-app.use(express.static("react-demo-app/dist"));
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
